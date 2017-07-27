d80c257209189059a56b1cec42e47191b2a8a5d2
essenchang <essenchang@gmail.com>
2017-07-27 19:06:00 +0800

    1st

---
+++ app/src/main/java/com/fishbowl/fish/fishcar/ListActivity.java
@@ -0,0 +1,17 @@
+package com.fishbowl.fish.fishcar;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by i0ac3032 on 2017/7/27.
+ */
+
+public class ListActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.list_activity);
+    }
+}
